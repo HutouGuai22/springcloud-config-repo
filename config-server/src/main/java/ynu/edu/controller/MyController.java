bf40c02481bc5b0ee5bfdb58f89f13b9b1ed4906
rendiandian <17387412019@163.com>
2025-06-08 23:26:18 +0800

    Update application name for testing dynamic refresh

--- config-server/src/main/java/ynu/edu/controller/MyController.java
+++ config-server/src/main/java/ynu/edu/controller/MyController.java
@@ -3,12 +3,16 @@ package ynu.edu.controller;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RefreshScope
 public class MyController {
-    @GetMapping("/message")
-    public String getMessage() {
-        return "Hello, World!";
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/app-name")
+    public String getAppName() {
+        return appName;
     }
 }
